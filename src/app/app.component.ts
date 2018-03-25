dfd3d2f001a1d57d6301408ed618efc2978261a1
bilel7 <31697833+bilel7@users.noreply.github.com>
2018-03-25 14:33:04 +0200

    nouveau dossier

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  image = 'http://www.khoahoclaptrinh.com/Content/Galleries/Training/CourseImage/imicrosoft-khoa-hoc-lap-trinh-angular4-web.jpg';
+}
